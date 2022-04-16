6432b4a695aab64545f67a1115ee22c9f5cc3bd0
AndreiMitioglo <mitioandrew2@gmail.com>
2022-04-16 12:00:55 -0500

    added company class

---
+++ src/encapsulation/Company.java
@@ -0,0 +1,44 @@
+package encapsulation;
+
+public class Company {
+
+    // create instance variables name, address, phone and encapsulate them
+
+    private String name;
+    private String address;
+    private String phone;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+  // override to string
+
+    @Override
+    public String toString() {
+        return "Company{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                '}';
+    }
+}
